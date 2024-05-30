99bf84925eaae1679f9770fd6239d133dcfd58e7
chanelcolgate <57675195+chanelcolgate@users.noreply.github.com>
2024-05-30 14:15:06 +0700

    update code footaball analysis

--- football_analysis/football_analysis/trackers/tracker.py
+++ football_analysis/football_analysis/trackers/tracker.py
@@ -10,6 +10,7 @@ from ultralyticsplus import YOLO, download_from_hub
 from football_analysis.utils.bbox_utils import (
     get_center_of_bbox,
     get_bbox_width,
+    get_foot_position,
 )
 
 
@@ -20,6 +21,17 @@ class Tracker:
         model_path = download_from_hub(hub_model_id)
         self.model = YOLO(model_path)
         self.tracker = sv.ByteTrack()
+
+    def add_position_to_track(self, tracks):
+        for object, object_tracks in tracks.items():
+            for frame_num, track in enumerate(object_tracks):
+                for track_id, track_info in track.items():
+                    bbox = track_info["bbox"]
+                    if object == "ball":
+                        position = get_center_of_bbox(bbox)
+                    else:
+                        position = get_foot_position(bbox)
+                    tracks[object][frame_num][track_id]["position"] = position
 
     def interpolate_ball_positions(self, ball_positions):
         ball_positions = [x.get(1, {}).get("bbox", []) for x in ball_positions]
@@ -105,7 +117,7 @@ class Tracker:
                 if cls_id == cls_names_inv["referee"]:
                     tracks["referees"][frame_num][track_id] = {"bbox": bbox}
 
-            for frame_detection in detection_with_tracks:
+            for frame_detection in detection_supervision:
                 bbox = frame_detection[0].tolist()
                 cls_id = frame_detection[3]
 
@@ -175,7 +187,45 @@ class Tracker:
         cv2.drawContours(frame, [triangle_points], 0, (0, 0, 0), 2)
         return frame
 
-    def draw_annotations(self, video_frames, tracks):
+    def draw_team_ball_control(self, frame, frame_num, team_ball_control):
+        # Draw a semi-transparent rectangle
+        overlay = frame.copy()
+        cv2.rectangle(overlay, (1350, 850), (1900, 970), (255, 255, 255), -1)
+        alpha = 0.4
+        cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
+
+        team_ball_control_till_frame = team_ball_control[: frame_num + 1]
+        # Get the number of time each team has ball control
+        team_1_num_frames = team_ball_control_till_frame[
+            team_ball_control_till_frame == 1
+        ].shape[0]
+        team_2_num_frames = team_ball_control_till_frame[
+            team_ball_control_till_frame == 2
+        ].shape[0]
+        team1 = team_1_num_frames / (team_1_num_frames + team_2_num_frames)
+        team2 = team_2_num_frames / (team_1_num_frames + team_2_num_frames)
+
+        cv2.putText(
+            frame,
+            f"Team 1 Ball Control: {team1*100:.2f}%",
+            (1400, 900),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 0, 0),
+            3,
+        )
+        cv2.putText(
+            frame,
+            f"Team 2 Ball Control: {team2*100:.2f}%",
+            (1400, 950),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 0, 0),
+            3,
+        )
+        return frame
+
+    def draw_annotations(self, video_frames, tracks, team_ball_control):
         output_video_frames = []
         for frame_num, frame in enumerate(video_frames):
             frame = frame.copy()
@@ -191,6 +241,11 @@ class Tracker:
                     frame, player["bbox"], color, track_id
                 )
 
+                if player.get("has_ball", False):
+                    frame = self.draw_triangle(
+                        frame, player["bbox"], (0, 0, 255)
+                    )
+
             # Draw Referees
             for _, referee in referee_dict.items():
                 frame = self.draw_ellipse(frame, referee["bbox"], (0, 255, 255))
@@ -198,6 +253,11 @@ class Tracker:
             # Draw ball
             for _, ball in ball_dict.items():
                 frame = self.draw_triangle(frame, ball["bbox"], (0, 255, 0))
+
+            # Draw Team Ball Control
+            frame = self.draw_team_ball_control(
+                frame, frame_num, team_ball_control
+            )
 
             output_video_frames.append(frame)
 
